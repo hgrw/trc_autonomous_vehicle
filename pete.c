516a3b8e8142eef9732a06a1365eeab6aad92a08
Peter Bell <pete@madeupdomain.com>
2018-02-17 14:48:07 +1100

    added code to read strings from Jetson over UART

--- pete.c
+++ pete.c
@@ -9,14 +9,15 @@
 #define REVERSE  5
 
 // Arduino pins
-#define RC_right_y_in   24
+#define RC_right_y_in   22
 #define RC_right_y_out  4
 #define RC_right_x_in   5
 #define RC_right_x_out  6
 #define RC_ignition_switch 10
+
 #define Ignition_Relay  22
 #define Battery_Relay   12
-#define Jetson_Boot     1111 // FIXME:
+#define Jetson_Boot     1111
 #define Throttle_Pin    2222
 #define Brake_Pin       3333
 #define Gear_Pin        4444
@@ -28,8 +29,21 @@
 #define GEAR_N 2
 #define GEAR_D 3
 
-// brake options
-#define BRAKE_FULL 255
+// brake constants
+#define BRAKE_ZERO 0
+#define BRAKE_MAX 180
+
+// throttle constants
+#define THROTTLE_ZERO 0
+#define THROTTLE_MAX 180
+
+// steering constants
+#define STEER_HARD_LEFT     0
+#define STEER_MIDDLE        90
+#define STEER_HARD_RIGHT    180
+
+// misc constants
+#define CRANK_TIME 1000
 
 //function prototypes
 void startEngine();
@@ -37,6 +51,7 @@ bool setGear(int);
 void setBrakes(int);
 void setSteering(int);
 void setThrottle(int);
+void RCToPWM(int);
 
 // init actuators
 Servo brakeServo;
@@ -73,6 +88,28 @@ void setup()
 
 void loop()
 {
+    // listen to RC controller
+    int ch_2_brake = pulseIn(RC_right_y_in, HIGH, 25000);
+    ch_2_brake = RCToPWM(ch_2);
+
+    int ch_8_ignition = pulseIn(RC_ignition_switch, HIGH, 25000);
+    ch_8_ignition = RCToPWM(ch_8);
+
+    // listen for Jetson
+    int jetson_throttle = 0;
+    int jetson_brake = 0;
+    int jetson_gear = GEAR_P;
+    int jetson_steer = STEER_MIDDLE;
+
+    // maybe not safe for scheduling
+    while (Serial.available() > 0) {
+        String incomingSignal = Serial.readString();
+
+        int jetson_throttle = parseJetson(incomingSignal,':',0);
+        int jetson_brake = parseJetson(incomingSignal,':',1);
+        int jetson_steer = parseJetson(incomingSignal,':',2);
+        // include stop commant
+     }
 
     // main state machine
     switch(state)
@@ -80,12 +117,9 @@ void loop()
         case PARK:
         {
             // actuate brakes on
-            setBrakes(BRAKE_FULL);
+            setBrakes(BRAKE_MAX);
 
-            // Check for start RC command
-            int ch_8 = pulseIn(CH_8_Pin, HIGH, 25000);
-
-            if (ch_8 > 1750)
+            if (ch_8_ignition > 1750)
             {
                 state = IGNITION;
             }
@@ -95,7 +129,7 @@ void loop()
         case IGNITION:
         {
             // brakes on
-            setBrakes(BRAKE_FULL);
+            setBrakes(BRAKE_MAX);
 
             // start engine and put in neutral
             startEngine();
@@ -106,7 +140,8 @@ void loop()
         }
         case NEUTRAL_RC:
         {
-            // listen for RC throttle, brake, steering, gear
+            setBrakes(ch_2_brake);
+
 
             // if RC asks for DRIVE_RC
                 // set brakes on
@@ -149,7 +184,7 @@ void loop()
         default:
         {
             // should not ever end up in this state
-            setBrakes(BRAKE_FULL);
+            setBrakes(BRAKE_MAX);
             setThrottle(THROTTLE_ZERO);
             break;
         }
@@ -160,7 +195,7 @@ void loop()
 void startEngine()
 {
     digitalWrite(Ignition_Relay, HIGH);
-    delay(1000);
+    delay(CRANK_TIME);
     digitalWrite(Ignition_Relay, LOW);
 }
 
@@ -184,6 +219,40 @@ void setSteering(int steer_angle)
 void setThrottle(int throttle_pos)
 {
 
+}
+
+int RCToPWM(int pulse)
+{
+  if ( pulse > 1000 ) {
+    pulse = map(pulse, 1000, 2000, -500, 500);
+    pulse = constrain(pulse, -255, 255);
+  } else {
+    pulse = 0;
+  }
+
+  // Anything in deadzone should stop the motor
+
+  return pulse;
+
+}
+
+String parseJetson(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    String val_string = found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+    int val_integer = val_string.toInt();
+
+    return val_integer;
 }
 
 // class Linda
